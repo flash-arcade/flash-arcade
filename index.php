881748c11d64894e997d7fe61687d2fedeba9430
Ernest Buffington <ernest.buffington@gmail.com>
2022-10-30 19:27:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -32,26 +32,26 @@
       Network Banner Ads                       v3.0.0       04/15/2021
  ************************************************************************/
 
-define('HOME_FILE', true);
-define('MODULE_FILE', true);
-$_SERVER['PHP_SELF'] = 'modules.php';
+define( 'HOME_FILE', true );
+define( 'MODULE_FILE', true );
+$_SERVER[ 'PHP_SELF' ] = 'modules.php';
 
-require_once(dirname(__FILE__).'/mainfile.php');
+require_once( dirname( __FILE__ ) . '/mainfile.php' );
 
 /*****[BEGIN]******************************************
  [ Mod:    Banner Ads                          v1.0.0 ]
  ******************************************************/
 global $prefix, $db, $admin_file, $httpref, $httprefmax;
-if (isset($_GET['op'])):
-	if($_GET['op'] == 'ad_click' && isset($_GET['bid'])):
-        $bid = intval($_GET['bid']);
-        list($clickurl) = $db->sql_ufetchrow("SELECT `clickurl` FROM `".$prefix."_banner` WHERE `bid`='$bid'", SQL_NUM);
-        if(!is_admin())
-        $db->sql_query("UPDATE `".$prefix."_banner` SET `clicks`=clicks+1 WHERE `bid`='$bid'");
-        redirect($clickurl);
-	else: 
-        exit('Illegal Operation');
-    endif;
+if ( isset( $_GET[ 'op' ] ) ):
+  if ( $_GET[ 'op' ] == 'ad_click' && isset( $_GET[ 'bid' ] ) ):
+    $bid = intval( $_GET[ 'bid' ] );
+list( $clickurl ) = $db->sql_ufetchrow( "SELECT `clickurl` FROM `" . $prefix . "_banner` WHERE `bid`='$bid'", SQL_NUM );
+if ( !is_admin() )
+  $db->sql_query( "UPDATE `" . $prefix . "_banner` SET `clicks`=clicks+1 WHERE `bid`='$bid'" );
+redirect( $clickurl );
+else :
+  exit( 'Illegal Operation' );
+endif;
 endif;
 /*****[END]********************************************
  [ Mod:    Banner Ads                          v1.0.0 ]
@@ -61,57 +61,57 @@ endif;
  [ Mod:    Network Banner Ads                          v1.0.0 ]#### 3/19/2021
  **************************************************************/
 global $network_prefix, $db2;
-if (isset($_GET['op'])):
-    if($_GET['op'] == 'ad_network_click' && isset($_GET['bid'])):
-        $bid = intval($_GET['bid']);
-        list($clickurl) = $db2->sql_ufetchrow("SELECT `clickurl` FROM `".$network_prefix."_banner` WHERE `bid`='$bid'", SQL_NUM);
-        if(!is_admin())
-        $db2->sql_query("UPDATE `".$network_prefix."_banner` SET `clicks`=clicks+1 WHERE `bid`='$bid'");
-        redirect($clickurl);
-	else: 
-        exit('Illegal Operation');
-    endif;
+if ( isset( $_GET[ 'op' ] ) ):
+  if ( $_GET[ 'op' ] == 'ad_network_click' && isset( $_GET[ 'bid' ] ) ):
+    $bid = intval( $_GET[ 'bid' ] );
+list( $clickurl ) = $db2->sql_ufetchrow( "SELECT `clickurl` FROM `" . $network_prefix . "_banner` WHERE `bid`='$bid'", SQL_NUM );
+if ( !is_admin() )
+  $db2->sql_query( "UPDATE `" . $network_prefix . "_banner` SET `clicks`=clicks+1 WHERE `bid`='$bid'" );
+redirect( $clickurl );
+else :
+  exit( 'Illegal Operation' );
+endif;
 endif;
 /*****[END]****************************************************
  [ Mod:    Network Banner Ads                          v1.0.0 ]#### 3/19/2021
  **************************************************************/
-  
+
 /*****[BEGIN]******************************************
  [ Mod:     Arcade                             v3.0.2 ]
  ******************************************************/
 // Arcade MOD - IBProSupport
-$arcade = get_query_var('act', 'get');
-$newscore = get_query_var('do', 'get');
+$arcade = get_query_var( 'act', 'get' );
+$newscore = get_query_var( 'do', 'get' );
 
-if($arcade == 'Arcade' && $newscore='newscore'):
-     $gamename = str_replace("\'","''",$HTTP_POST_VARS['gname']);
-     $gamename = preg_replace(array('#&(?!(\#[0-9]+;))#', '#<#', '#>#'), array('&amp;', '&lt;', '&gt;'),$gamename);
-     $gamescore = intval($HTTP_POST_VARS['gscore']);
+if ( $arcade == 'Arcade' && $newscore = 'newscore' ):
+  $gamename = str_replace( "\'", "''", $HTTP_POST_VARS[ 'gname' ] );
+$gamename = preg_replace( array( '#&(?!(\#[0-9]+;))#', '#<#', '#>#' ), array( '&amp;', '&lt;', '&gt;' ), $gamename );
+$gamescore = intval( $HTTP_POST_VARS[ 'gscore' ] );
 
-      //Get Game ID
-      $row = $db->sql_ufetchrow("SELECT game_id FROM ".$prefix."_bbgames WHERE game_scorevar='$gamename'");
-      $gid = intval($row['game_id']);
+//Get Game ID
+$row = $db->sql_ufetchrow( "SELECT game_id FROM " . $prefix . "_bbgames WHERE game_scorevar='$gamename'" );
+$gid = intval( $row[ 'game_id' ] );
 
-      $ThemeSel = get_theme();
+$ThemeSel = get_theme();
 
-      print '<link rel="StyleSheet" href="themes/"'.$ThemeSel.'"/style/style.css" type="text/css">'."\n";
-      print '<form method="post" name="ibpro_score" action="modules.php?name=Forums&amp;file=proarcade&amp;valid=X&amp;gpaver=GFARV2">'."\n";
-      print '<input type=hidden name="vscore" value="'.$gamescore.'">'."\n";
-      print '<input type=hidden name="gid" value="'.$gid.'">'."\n";
-      print '</form>'."\n";
+print '<link rel="StyleSheet" href="themes/"' . $ThemeSel . '"/style/style.css" type="text/css">' . "\n";
+print '<form method="post" name="ibpro_score" action="modules.php?name=Forums&amp;file=proarcade&amp;valid=X&amp;gpaver=GFARV2">' . "\n";
+print '<input type=hidden name="vscore" value="' . $gamescore . '">' . "\n";
+print '<input type=hidden name="gid" value="' . $gid . '">' . "\n";
+print '</form>' . "\n";
 
-      print '<script type="text/javascript">'."\n";
-      print 'window.onload = function(){document.forms["ibpro_score"].submit()}'."\n";
-      print '</script>'."\n";
+print '<script type="text/javascript">' . "\n";
+print 'window.onload = function(){document.forms["ibpro_score"].submit()}' . "\n";
+print '</script>' . "\n";
 exit;
 
 endif;
 /*****[END]********************************************
  [ Mod:     Arcade                             v3.0.2 ]
  ******************************************************/
- 
-if (isset($_GET['url']) && is_admin())
-redirect($_GET['url']);
+
+if ( isset( $_GET[ 'url' ] ) && is_admin() )
+  redirect( $_GET[ 'url' ] );
 
 $module_name = main_module();
 
@@ -119,37 +119,36 @@ $module_name = main_module();
  [ Mod:     Lock Modules                       v1.0.0 ]
  ******************************************************/
 global $lock_modules;
-if(($lock_modules && $module_name != 'Your_Account') && !is_admin() && !is_user()) 
-include(NUKE_MODULES_DIR.'Your_Account/index.php');
+if ( ( $lock_modules && $module_name != 'Your_Account' ) && !is_admin() && !is_user() )
+  include( NUKE_MODULES_DIR . 'Your_Account/index.php' );
 /*****[END]********************************************
  [ Mod:     Lock Modules                       v1.0.0 ]
  ******************************************************/
 
-$mop = (!isset($mop)) ? 'modload' : trim($mop);
-$mod_file = (!isset($mod_file)) ? 'index' : trim($mod_file);
-$file = (isset($_REQUEST['file'])) ? trim($_REQUEST['file']) : 'index';
+$mop = ( !isset( $mop ) ) ? 'modload' : trim( $mop );
+$mod_file = ( !isset( $mod_file ) ) ? 'index' : trim( $mod_file );
+$file = ( isset( $_REQUEST[ 'file' ] ) ) ? trim( $_REQUEST[ 'file' ] ) : 'index';
 
-if (!isset($modpath)) 
-$modpath = ''; 
+if ( !isset( $modpath ) )
+  $modpath = '';
 
-if (stristr($file,"..") || stristr($mod_file,"..") || stristr($mop,"..")):
-/*****[BEGIN]******************************************
- [ Base:     Evolution Functions               v1.5.0 ]
- ******************************************************/
-    log_write('error', 'Inappropriate module path was used', 'Hack Attempt');
+if ( stristr( $file, ".." ) || stristr( $mod_file, ".." ) || stristr( $mop, ".." ) ):
+  /*****[BEGIN]******************************************
+   [ Base:     Evolution Functions               v1.5.0 ]
+   ******************************************************/
+  log_write( 'error', 'Inappropriate module path was used', 'Hack Attempt' );
 /*****[END]********************************************
  [ Base:     Evolution Functions               v1.5.0 ]
  ******************************************************/
-    die("You are so cool...");
-else:
-    $module = $db->sql_ufetchrow('SELECT `blocks` FROM `'.$prefix.'_modules` WHERE `title`="'.$module_name.'"');
-	$modpath = NUKE_MODULES_DIR.$module_name."/$file.php";
-	if (file_exists($modpath)):
-		$showblocks = $module['blocks'];
-		unset($module, $error);
-		require($modpath);
-    else:
-        DisplayError((is_admin()) ? "<strong>"._HOMEPROBLEM."</strong><br /><br />[ <a href=\"".$admin_file.".php?op=modules\">"._ADDAHOME."</a> ]" : _HOMEPROBLEMUSER);
-    endif;
+die( "You are so cool..." );
+else :
+  $module = $db->sql_ufetchrow( 'SELECT `blocks` FROM `' . $prefix . '_modules` WHERE `title`="' . $module_name . '"' );
+$modpath = NUKE_MODULES_DIR . $module_name . "/$file.php";
+if ( file_exists( $modpath ) ):
+  $showblocks = $module[ 'blocks' ];
+unset( $module, $error );
+require( $modpath );
+else :
+  DisplayError( ( is_admin() ) ? "<strong>" . _HOMEPROBLEM . "</strong><br /><br />[ <a href=\"" . $admin_file . ".php?op=modules\">" . _ADDAHOME . "</a> ]" : _HOMEPROBLEMUSER );
 endif;
-
+endif;
